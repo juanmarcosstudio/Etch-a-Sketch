f5650e1f1ddd61b08bfdbee229a83d4f618fd6b4
Jhon Mark <jhonmarktamayo134@gmail.com>
2024-11-04 14:17:47 +0700

    add: container

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.getElementById('grid-container');
+
+
+function createGrid(size) {
+    CSSContainerRule.innerHTML = '';
+
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div')
+    }
+}
